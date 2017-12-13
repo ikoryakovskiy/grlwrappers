11d6932d07b9d69c644d52a2735acf59987df720
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-12-13 15:52:31 +0100

    Thread-safe implementation of py_env

--- grlgym/envs/grl.py
+++ grlgym/envs/grl.py
@@ -1,30 +1,30 @@
 import gym
-import py_env
+from py_env import PyEnv
 from gym.spaces import Box
 
 class GRLEnv(gym.Env):
 
     def __init__(self, config_file, test = 0):
-        od, o_min, o_max, ad, a_min, a_max = py_env.init(config_file)
+        self.env = PyEnv()
+        od, o_min, o_max, ad, a_min, a_max = self.env.init(config_file)
         self.observation_space = Box(o_min, o_max)
         self.action_space = Box(a_min, a_max)
         self.test = test
-    
+
     def _seed(self, seed):
         """ Starting the environment """
         if not seed:
             seed = 0
-        return py_env.seed(seed)
+        return self.env.seed(seed)
     
     def _reset(self):
         """ Starting the environment """
-        return py_env.start(self.test)
+        return self.env.start(self.test)
 
     def _step(self, action):
         """ Next observation """
-        obs, reward, terminal, tau = py_env.step(action)
+        obs, reward, terminal, tau = self.env.step(action)
         return obs, reward, terminal, {}
         
     def _close(self):
-        py_env.fini()
-
+        self.env.fini()
--- tests/py_env_test.py
+++ tests/py_env_test.py
@@ -8,30 +8,66 @@ Created on Mon Dec 11 16:20:33 2017
 import numpy as np
 from grlgym.envs.grl import GRLEnv
 
-# Check outcome of policies. It should be different
-# if the py_env library is loaded in a separate space
-env = GRLEnv("rbdl_py_balancing.yaml", test = 0)
+def main():
 
-env.seed(1)
-s = env.reset()
-print('env 0:  %s' % [x for x in s])
-
-eval_env = GRLEnv("rbdl_py_balancing.yaml", test = 1)
-se = eval_env.reset()
-print('eval_env  0:  %s' % [x for x in se])
-
-a = np.zeros(env.action_space.shape) #+ np.array([0, 0] + [0.1]*7)
-
-for i in range(1000):
-  s, rw, tt, tau = env.step(a)
-  print('env:')
-  print('  %s' % ['%5.5f' % val for val in s])
-  print('  %s' % [rw, tt, tau])
+  # Learning environment
+  env = GRLEnv("rbdl_py_balancing.yaml", test = 0)
+  env.seed(1)
+  env.reset()
+  a = np.zeros(env.action_space.shape) #+ np.array([0, 0] + [0.1]*7)
+  s_fin = run(env, a)
   
-  se = eval_env.reset()
+  # Testing environment
+  eval_env = GRLEnv("rbdl_py_balancing.yaml", test = 1)
+  s_eval_fin = run(eval_env, a)
   
-  if tt:
+  # Seeds are different, so result should be different
+  assert((s_fin != s_eval_fin).any())
+
+  # Ensure the thread-safetness of the lib
+  # Run with reset
+  reset_run(env, eval_env, a)
+
+  # Run concurrently  
+  concurrent_run(env, eval_env, a)
+  
+  env.close()
+  eval_env.close()
+
+
+def run(e, a):
+  s = e.reset()
+  print('%s' % [x for x in s])
+  for i in range(1000):
+    s, rw, tt, tau = e.step(a)
+    print('env:')
+    print('  %s' % ['%5.5f' % val for val in s])
+    print('  %s' % [rw, tt, tau])
+    if tt:
+      break;
+  return s 
+  
+def reset_run(e1, e2, a):
+  s1_0 = e1.reset()
+  e2.reset()
+  for i in range(1000):
+    s1_1, rw, tt, tau = e1.step(a)
+    e2.reset()
+    assert((s1_0 != s1_1).any())
+    if tt:
+      break;
+  
+def concurrent_run(e1, e2, a):
+  s1_0 = e1.reset()
+  s2_0 = e2.reset()
+  assert((s1_0 != s2_0).any())
+  for i in range(1000):
+    s1_1, rw, tt1, tau = e1.step(a)
+    s2_1, rw, tt2, tau = e2.step(a)
+    assert((s1_1 != s2_1).any())
+    if tt1 or tt2:
       break;
 
-env.close()
-eval_env.close()
+
+if __name__ == "__main__":
+  main()

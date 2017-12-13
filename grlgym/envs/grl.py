7c1186a92734830670db46ac5aae0bc5e5cd5972
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-12-13 14:13:07 +0100

    Not thread-safe code yet

--- grlgym/envs/grl.py
+++ grlgym/envs/grl.py
@@ -4,14 +4,21 @@ from gym.spaces import Box
 
 class GRLEnv(gym.Env):
 
-    def __init__(self, config_file):
+    def __init__(self, config_file, test = 0):
         od, o_min, o_max, ad, a_min, a_max = py_env.init(config_file)
         self.observation_space = Box(o_min, o_max)
         self.action_space = Box(a_min, a_max)
-
+        self.test = test
+    
+    def _seed(self, seed):
+        """ Starting the environment """
+        if not seed:
+            seed = 0
+        return py_env.seed(seed)
+    
     def _reset(self):
         """ Starting the environment """
-        return py_env.start(0)
+        return py_env.start(self.test)
 
     def _step(self, action):
         """ Next observation """
--- tests/py_env_test.py
+++ tests/py_env_test.py
@@ -6,21 +6,32 @@ Created on Mon Dec 11 16:20:33 2017
 @author: Ivan Koryakovskiy <i.koryakovskiy@gmail.com>
 """
 import numpy as np
-
 from grlgym.envs.grl import GRLEnv
-env = GRLEnv("rbdl_py_balancing.yaml")
 
+# Check outcome of policies. It should be different
+# if the py_env library is loaded in a separate space
+env = GRLEnv("rbdl_py_balancing.yaml", test = 0)
+
+env.seed(1)
 s = env.reset()
+print('env 0:  %s' % [x for x in s])
 
-print([x for x in s])
+eval_env = GRLEnv("rbdl_py_balancing.yaml", test = 1)
+se = eval_env.reset()
+print('eval_env  0:  %s' % [x for x in se])
 
 a = np.zeros(env.action_space.shape) #+ np.array([0, 0] + [0.1]*7)
 
 for i in range(1000):
   s, rw, tt, tau = env.step(a)
-  print(['%5.5f' % val for val in s])
-  print((rw, tt, tau))
+  print('env:')
+  print('  %s' % ['%5.5f' % val for val in s])
+  print('  %s' % [rw, tt, tau])
+  
+  se = eval_env.reset()
+  
   if tt:
       break;
 
 env.close()
+eval_env.close()

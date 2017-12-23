6f742223b2db57c211afe08e98c95978b3308d4a
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-12-23 15:29:34 +0100

    Adding reconfigure option

--- grlgym/envs/grl.py
+++ grlgym/envs/grl.py
@@ -17,7 +17,7 @@ class GRLEnv(gym.Env):
         if not seed:
             seed = 0
         self.env.seed(seed)
-    
+
     def _reset(self):
         """ Starting the environment """
         obs = self.env.start(int(self.test))
@@ -28,18 +28,20 @@ class GRLEnv(gym.Env):
         # sometimes provided action is float32, but only float64 is accepted
         obs, reward, terminal, info = self.env.step(action.astype(np.float64))
         return obs, reward, terminal, info
-        
+
     def _close(self):
         self.env.fini()
-        
+
     # Own methods
     def set_test(self, test=False):
         """ Use before reset() to select type of environment: learning or testing """
         self.test = test
-        
+
     def report(self, report='test'):
         """ Select in which episodes report (info) is requested """
         idx = ['learn', 'test', 'all'].index(report)
         self.env.report(idx)
-        
-       
+
+    def reconfigure(self, d=None):
+        """ Reconfigure the environemnt using the dict """
+        self.env.reconfigure(d)
